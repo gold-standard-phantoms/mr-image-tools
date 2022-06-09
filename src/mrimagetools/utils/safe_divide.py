16c61ec6c96aac2ffc8e76b5e5985e51ebd440a4
Léo-Paul Hauet <leo-paul.hauet@goldstandardphantoms.com>
2022-06-09 19:08:16 +0200

    MIT-6: Adding scalar/integer/complex support

--- src/mrimagetools/utils/safe_divide.py
+++ src/mrimagetools/utils/safe_divide.py
@@ -1,22 +1,80 @@
 """safe_divide utilities"""
+from typing import Union
+
 import numpy as np
 
+InputType = Union[np.ndarray, float, int, complex]
 
-def safe_divide(num: np.ndarray, div: np.ndarray) -> np.ndarray:
+
+def safe_divide(numerator: InputType, divisor: InputType) -> InputType:
     """safe divide function - does the division of each element
-    from num and div except for the 0 values of div, in which case
+    from numerator and divisor except for the 0 values of divisor, in which case
     it'll just return 0
     Arrays need to be broadcastable
+    It can also take floats and integers as inputs
 
-    :param num: the numerator for the safe division
-    :type num: numpy ndarray
-    :param div: the divisor for the safe division
-    :type div: numpy ndarray
-    :return: return the safe division, ignore the 0 values of div
-    :rtype: numpy ndarray
+    :param numerator: the numeratorerator for the safe division
+    :type numerator: numpy ndarray or float or int or complex
+    :param divisor: the divisor for the safe division
+    :type divisor: numpy ndarray or float or int or complex
+    :return: return the safe division, ignore the 0 values of divisor
+    :rtype: numpy ndarray or float or int or complex
     :raises ValueError: ValueError from numpy true_divide if
         arrays are not broadcastable
     """
-    return np.true_divide(
-        num, div, out=np.zeros_like(div), where=div > 0, dtype=np.float32
+
+    if np.iscomplexobj(divisor) or np.iscomplexobj(numerator):
+        if isinstance(divisor, np.ndarray) and isinstance(numerator, np.ndarray):
+            return np.true_divide(
+                numerator,
+                divisor,
+                where=divisor > 0,
+                dtype=np.complex128,
+                casting="unsafe",
+            )
+
+        if not isinstance(divisor, np.ndarray) and not isinstance(
+            numerator, np.ndarray
+        ):
+            if divisor == 0:
+                return 0 + 0j
+            return np.true_divide(numerator, divisor, dtype=np.complex128)
+        if not isinstance(divisor, np.ndarray):
+            if divisor == 0:
+                return np.zeros_like(numerator, dtype=np.complex128)
+            return np.true_divide(numerator, divisor, dtype=np.complex128)
+        if not isinstance(numerator, np.ndarray):
+            return np.true_divide(
+                numerator, divisor, where=divisor > 0, dtype=np.complex128
+            )
+
+    if isinstance(divisor, np.ndarray) and isinstance(numerator, np.ndarray):
+        return np.true_divide(
+            numerator,
+            divisor,
+            out=np.zeros_like(divisor),
+            where=divisor > 0,
+            dtype=np.float32,
+        )
+
+    if not isinstance(divisor, np.ndarray) and not isinstance(numerator, np.ndarray):
+        if divisor == 0:
+            return 0
+        return np.true_divide(numerator, divisor, dtype=np.float32)
+    if not isinstance(divisor, np.ndarray):
+        if divisor == 0:
+            return np.zeros_like(numerator, dtype=np.float32)
+        return np.true_divide(numerator, divisor, dtype=np.float32)
+    if not isinstance(numerator, np.ndarray):
+        return np.true_divide(
+            numerator,
+            divisor,
+            out=np.zeros_like(divisor),
+            where=divisor > 0,
+            dtype=np.float32,
+        )
+    type_numerator = type(numerator)
+    type_divisor = type(divisor)
+    raise TypeError(
+        f"Input type not supported by safe_divide (numerator is {type_numerator} and divisor is {type_divisor}"
     )
--- src/mrimagetools/utils/test_safe_divide.py
+++ src/mrimagetools/utils/test_safe_divide.py
@@ -1,6 +1,4 @@
 """safe_divide.py tests"""
-from multiprocessing.sharedctypes import Value
-
 import numpy as np
 import pytest
 
@@ -14,6 +12,12 @@ def fixture_input_num() -> np.ndarray:
 
 
 @pytest.fixture()
+def fixture_input_num_complex() -> np.ndarray:
+    """A test complex numerator"""
+    return np.array([1 + 1j, 2 + 2j, 3 + 3j, 4 + 4j], dtype=np.complex128)
+
+
+@pytest.fixture()
 def fixture_input_0_num() -> np.ndarray:
     """A test 0 numerator"""
     return np.array([0, 0, 0, 0], dtype=np.float32)
@@ -23,6 +27,12 @@ def fixture_input_0_num() -> np.ndarray:
 def fixture_input_div() -> np.ndarray:
     """A test divisor"""
     return np.array([1, 2, 3, 4], dtype=np.float32)
+
+
+@pytest.fixture()
+def fixture_input_div_complex() -> np.ndarray:
+    """A test complex divisor"""
+    return np.array([1 + 1j, 2 + 2j, 3 + 3j, 4 + 4j], dtype=np.complex128)
 
 
 @pytest.fixture()
@@ -63,3 +73,59 @@ def test_non_broadcastable(fixture_input_div: np.ndarray):
     """test non broadcastable error"""
     with pytest.raises(ValueError):
         safe_divide(np.array([1, 2, 3]), fixture_input_div)
+
+
+def test_div_int(fixture_input_num: np.ndarray):
+    """test numpy array division by int"""
+    assert np.array_equal(safe_divide(fixture_input_num, 2), [1 / 2, 1, 3 / 2, 2])
+
+
+def test_num_int(fixture_input_div: np.ndarray):
+    """test numpy array division of int by array"""
+    assert np.array_equal(safe_divide(12, fixture_input_div), [12, 6, 4, 3])
+
+
+def test_2_int():
+    """test the division of two int"""
+    assert safe_divide(8, 2) == 4
+
+
+def test_complex_div_comple_num(
+    fixture_input_num_complex: np.ndarray, fixture_input_div_complex: np.ndarray
+):
+    """test the division by a complex array with complex numerator"""
+    assert np.array_equal(
+        safe_divide(fixture_input_num_complex, fixture_input_div_complex),
+        fixture_input_num_complex / fixture_input_div_complex,
+    )
+
+
+def test_complex_div(
+    fixture_input_num: np.ndarray, fixture_input_div_complex: np.ndarray
+):
+    """test the division by a complex array with int array numerator"""
+    assert np.array_equal(
+        safe_divide(fixture_input_num, fixture_input_div_complex),
+        fixture_input_num / fixture_input_div_complex,
+    )
+
+
+def test_complex_num(
+    fixture_input_num_complex: np.ndarray, fixture_input_div: np.ndarray
+):
+    """test the division with a complex array numerator and int array divisor"""
+    assert np.array_equal(
+        safe_divide(fixture_input_num_complex, fixture_input_div),
+        fixture_input_num_complex / fixture_input_div,
+    )
+
+
+def test_complex_by_0_stays_complex():
+    """test that a complex number divided by 0 stays complex even if still 0"""
+    assert safe_divide(4 + 4j, 0) == 0j
+
+
+def test_wrong_type():
+    """test to see if the TypeError is raised if not an InputType is inputed"""
+    with pytest.raises(TypeError):
+        safe_divide("wrong_input", 1)
