bdcdc8fcfe12384a395bfdf7544d38ade539cccc
Tom Hampshire <tom.hampshire@goldstandardphantoms.com>
2022-06-29 15:50:47 +0100

    MIT-26: Nifti data type field

--- src/mrimagetools/validators/fields.py
+++ src/mrimagetools/validators/fields.py
@@ -44,3 +44,81 @@ class UnitField(str):
         if not is_a_unit(value):
             raise ValueError(f"{value} is an invalid unit.")
         return cls(value)
+
+
+NiftiDataType = Literal[
+    "none",
+    "binary",
+    "uint8",
+    "int16",
+    "int32",
+    "float32",
+    "complex64",
+    "float64",
+    "RGB",
+    "all",
+    "int8",
+    "uint16",
+    "uint32",
+    "int64",
+    "uint64",
+    "float128",
+    "complex128",
+    "complex256",
+    "RGBA",
+]
+NIFTI_DATATYPES: Final[Tuple[NiftiDataType, ...]] = typing.get_args(NiftiDataType)
+# nifti datatypes (adapted from nibabel.nifti1._dtdefs)
+NIFTI_DATATYPE_MAP: Final[Dict[NiftiDataType, int]] = {
+    "none": 0,
+    "binary": 1,
+    "uint8": 2,
+    "int16": 4,
+    "int32": 8,
+    "float32": 16,
+    "complex64": 32,
+    "float64": 64,
+    "RGB": 128,
+    "all": 255,
+    "int8": 256,
+    "uint16": 512,
+    "uint32": 768,
+    "int64": 1024,
+    "uint64": 1280,
+    "float128": 1536,
+    "complex128": 1792,
+    "complex256": 2048,
+    "RGBA": 2304,
+}
+
+
+class NiftiDataTypeField(str):
+    """A nifti data type. Must be one of NiftiDataType."""
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def __modify_schema__(cls, field_schema: Dict[str, Any]) -> None:
+        field_schema.update(
+            # the full list of allowed types
+            examples=NIFTI_DATATYPES
+        )
+
+    @classmethod
+    def validate(cls, value: Any) -> "NiftiDataTypeField":
+        """Validate the NIFTI data type"""
+        if not isinstance(value, str):
+            raise TypeError(f"String required and a {type(value)} was supplied.")
+
+        if value not in NIFTI_DATATYPES:
+            raise ValueError(
+                f"{value} is an invalid NIFTI datatype. Options are: {NIFTI_DATATYPES}"
+            )
+        return cls(value)
+
+    @property
+    def type_code(self) -> int:
+        """Return the associate NIFTI data type code"""
+        return NIFTI_DATATYPE_MAP[self]  # type: ignore
--- src/mrimagetools/validators/test_fields.py
+++ src/mrimagetools/validators/test_fields.py
@@ -1,7 +1,8 @@
+"""Tests for fields.py"""
 import pytest
 
 from mrimagetools.validators.checkers import is_a_unit
-from mrimagetools.validators.fields import UnitField
+from mrimagetools.validators.fields import NiftiDataTypeField, UnitField
 
 
 def test_units() -> None:
@@ -21,3 +22,25 @@ def test_units() -> None:
         assert not is_a_unit(unit)
         with pytest.raises(ValueError):
             UnitField.validate(unit)
+
+
+def test_nifti_data_type_field() -> None:
+    """Test the nifti data type field"""
+    for data_type in ["int8", "float32", "complex128"]:
+        NiftiDataTypeField.validate(data_type)  # no exception raised
+
+    # bad type
+    for data_type in [1, Exception(), str]:
+        with pytest.raises(TypeError):
+            NiftiDataTypeField.validate(data_type)
+
+    # bad value
+    for data_type in ["foobar", "foo"]:
+        with pytest.raises(ValueError):
+            NiftiDataTypeField.validate(data_type)
+
+
+def test_get_nifti_data_type_codes() -> None:
+    """Check that we can return the integer codes for NIFTI data type strings"""
+    for data_type in [("none", 0), ("float32", 16), ("uint64", 1280)]:
+        assert NiftiDataTypeField(data_type[0]).type_code == data_type[1]
