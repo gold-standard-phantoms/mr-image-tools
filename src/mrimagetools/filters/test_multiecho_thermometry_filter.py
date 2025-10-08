128355fecff5c1d96ccd6676f41895d7b851eba7
Aaron Oliver-Taylor <aaron.oliver-taylor@goldstandardphantoms.com>
2025-10-08 12:24:50 +0100

    feat(filter): voxelwise thermometry implemented
    - implemented voxelwise thermometry in multiecho_thermometry_filter
      this allows temperature estimation on a per-voxel basis, and calculation
      of region-wise temperature using uncertainties as weights

--- src/mrimagetools/filters/test_multiecho_thermometry_filter.py
+++ src/mrimagetools/filters/test_multiecho_thermometry_filter.py
@@ -1,7 +1,9 @@
 """Tests for multiecho thermometry filter functions."""
 
 import pdb
-from typing import Dict, List, Tuple, Union
+import re
+from turtle import rt
+from typing import Dict, List, Literal, Tuple, Union
 
 import nibabel as nib
 import numpy as np
@@ -9,6 +11,7 @@ import pandas as pd
 import pytest
 
 from mrimagetools.filters.multiecho_thermometry_filter import (
+    AnalysisMethod,
     MultiEchoThermometryParameters,
     calculate_df_from_temperature,
     calculate_temperature_from_df,
@@ -272,12 +275,10 @@ def thermometry_test_volume() -> (
         np.ndarray,
         NiftiImageContainer,
         NiftiImageContainer,
+        NiftiImageContainer,
         np.ndarray,
-        float,
-        float,
-        float,
-        float,
-        float,
+        List[int],
+        List[float],
     ]
 ):
     region_id = [1, 2, 3]
@@ -289,9 +290,9 @@ def thermometry_test_volume() -> (
     r2star_1 = 1.0  # 1/s
     r2star_2 = 2.0  # 1/s
     dphi_deg = 45.0
-    nx, ny, nz = 3, 2, 2  # small volume for testing
+    nx, ny, nz = 3, 6, 4  # small volume for testing
 
-    # create a small 3x3 image with 3 regions
+    # create a small 3x6x4 image with 3 regions
     true_temperature_map: np.ndarray = np.zeros((nx, ny, nz), dtype=np.float64)
     for i, temp in enumerate(region_temperature_celsius):
         true_temperature_map[i, :, :] = temp
@@ -319,6 +320,14 @@ def thermometry_test_volume() -> (
     multi_echo_image = NiftiImageContainer(
         nifti_img=nib.nifti1.Nifti1Image(multi_echo_data, affine=np.eye(4))
     )
+    # add some noise to create a noisy version
+    rng = np.random.default_rng(43)
+    noisy_multi_echo_image = NiftiImageContainer(
+        nifti_img=nib.nifti1.Nifti1Image(
+            multi_echo_data + rng.normal(0, 0.001, multi_echo_data.shape),
+            affine=np.eye(4),
+        )
+    )
     segmentation_image = NiftiImageContainer(
         nifti_img=nib.nifti1.Nifti1Image(segmentation_mask, affine=np.eye(4))
     )
@@ -328,27 +337,23 @@ def thermometry_test_volume() -> (
         true_temperature_map,
         segmentation_image,
         multi_echo_image,
+        noisy_multi_echo_image,
         echo_times,
-        amplitude_1,
-        amplitude_2,
-        r2star_1,
-        r2star_2,
-        dphi_deg,
+        region_id,
+        region_temperature_celsius,
     )
 
 
-def test_multiecho_thermometry_filter_function(
+def test_multiecho_thermometry_filter_function_regionwise(
     thermometry_test_volume: Tuple[
         float,
         np.ndarray,
         NiftiImageContainer,
         NiftiImageContainer,
+        NiftiImageContainer,
         np.ndarray,
-        float,
-        float,
-        float,
-        float,
-        float,
+        List[int],
+        List[float],
     ]
 ) -> None:
     (
@@ -356,14 +361,13 @@ def test_multiecho_thermometry_filter_function(
         true_temperature_map,
         segmentation_image,
         multi_echo_image,
+        _,
         echo_times,
-        amplitude_1,
-        amplitude_2,
-        r2star_1,
-        r2star_2,
-        dphi_deg,
+        region_id,
+        region_temperature_celsius,
     ) = thermometry_test_volume
 
+    # test regionwise AnalysisMethod
     results, temperature_image = multiecho_thermometry_filter(
         parameters=MultiEchoThermometryParameters(
             image_multiecho=multi_echo_image,
@@ -374,7 +378,104 @@ def test_multiecho_thermometry_filter_function(
         )
     )
 
+    assert isinstance(temperature_image, NiftiImageContainer)
+    # compare results to true temperature map
+
     np.testing.assert_array_almost_equal(
         temperature_image.image,
         true_temperature_map,
+    )
+
+    # compare results to expected region temperatures
+    for res in results:
+        # check the region id is in the expected list
+        assert res.region_id in region_id
+        # check the region temperature is close to the expected value
+        assert np.isclose(
+            res.region_mean_temperature,
+            region_temperature_celsius[region_id.index(res.region_id)],
+        )
+        # check that the uncertainty is close to zero for clean data
+        assert np.isclose(res.region_temperature_uncertainty[0], 0.00, atol=1e-6)
+        # check that the uncertainty k-value is 1
+        assert res.region_temperature_uncertainty[1] == 1
+        # check that res.region_size is correct
+        assert res.region_size == np.sum(segmentation_image.image == res.region_id)
+        # check that the fit r_squared is close to 1
+        assert np.allclose(res.r_squared, 1.0)
+        # value arrays should equal the scalar values for regionwise analysis
+        assert np.allclose(res.region_temperature_values, res.region_mean_temperature)
+        # uncertainty arrays should equal the scalar values for regionwise analysis
+        assert (
+            res.region_temperature_uncertainty_values
+            == res.region_temperature_uncertainty[0]
+        ).all()
+
+
+def test_multiecho_thermometry_filter_function_voxelwise(
+    thermometry_test_volume: Tuple[
+        float,
+        np.ndarray,
+        NiftiImageContainer,
+        NiftiImageContainer,
+        NiftiImageContainer,
+        np.ndarray,
+        List[int],
+        List[float],
+    ]
+) -> None:
+    (
+        magnetic_field_tesla,
+        true_temperature_map,
+        segmentation_image,
+        multi_echo_image,
+        noisy_multi_echo_image,
+        echo_times,
+        region_id,
+        region_temperature_celsius,
+    ) = thermometry_test_volume
+
+    # test voxelwise AnalysisMethod with clean data
+    results, temperature_image = multiecho_thermometry_filter(
+        parameters=MultiEchoThermometryParameters(
+            image_multiecho=multi_echo_image,
+            image_segmentation=segmentation_image,
+            echo_times=echo_times.tolist(),
+            magnetic_field_tesla=magnetic_field_tesla,
+            analysis_method="voxelwise",
+        )
+    )
+    assert isinstance(temperature_image, NiftiImageContainer)
+
+    np.testing.assert_array_almost_equal(temperature_image.image, true_temperature_map)
+
+    # compare results to expected region temperatures
+    for res in results:
+        # check the region id is in the expected list
+        assert res.region_id in region_id
+        # check the region temperature is close to the expected value
+        assert np.isclose(
+            res.region_mean_temperature,
+            region_temperature_celsius[region_id.index(res.region_id)],
+        )
+        # check that the uncertainty is close to zero for clean data
+        assert np.isclose(res.region_temperature_uncertainty[0], 0.00, atol=1e-6)
+        # check that the uncertainty k-value is 1
+        assert res.region_temperature_uncertainty[1] == 1
+        # check that res.region_size is correct
+        assert res.region_size == np.sum(segmentation_image.image == res.region_id)
+        # check that the fit r_squared is close to 1
+        assert np.allclose(res.r_squared, 1.0)
+        # value arrays should equal the scalar values for regionwise analysis
+        assert np.allclose(res.region_temperature_values, res.region_mean_temperature)
+
+    # now test with noisy data
+    results, noisy_temperature_image = multiecho_thermometry_filter(
+        parameters=MultiEchoThermometryParameters(
+            image_multiecho=noisy_multi_echo_image,
+            image_segmentation=segmentation_image,
+            echo_times=echo_times.tolist(),
+            magnetic_field_tesla=magnetic_field_tesla,
+            analysis_method="voxelwise",
+        )
     )
